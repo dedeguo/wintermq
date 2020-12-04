035ce4087e639da76856b7ed492a4f404ad8b54b
dedeguo <chende2018@gmail.com>
2020-12-04 09:01:03 +0800

    todo

---
+++ example/src/main/java/org/example/wintermq/TestDemo.java
@@ -0,0 +1,11 @@
+package org.example.wintermq;
+
+/**
+ * @author chende
+ * @version 1.0.0
+ * @ClassName TestDemo.java
+ * @Description TODO
+ * @createTime 2020年12月04日 08:59:00
+ */
+public class TestDemo {
+}
